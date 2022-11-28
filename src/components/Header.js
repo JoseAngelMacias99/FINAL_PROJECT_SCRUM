00e61714f2c96bcc697ac93d6be04ab9c45061fc
Jose Angel Macías <85316960+JoseAngelMacias99@users.noreply.github.com>
2022-11-28 10:58:03 -0600

    New changes login
    colors

--- src/components/Header.js
+++ src/components/Header.js
@@ -32,8 +32,8 @@ const Header = () => {
         <div className="container">
           <div className="row">
             <div className="col-md-6 d-flex align-items-center display-none">
-              <p>+255 768 356 890</p>
-              <p>info@zpunet.com</p>
+              <p>+52449123498</p>
+              <p>Goats_U_T_R_@utr.com</p>
             </div>
             <div className=" col-12 col-lg-6 justify-content-center justify-content-lg-end d-flex align-items-center">
               <Link to="">
